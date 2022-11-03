8eb4fd856a85915185d5814ce20f53ffaf53a428
higoraln <Higor Allan>
2022-11-03 20:49:44 -0300

    accebilit

--- src/components/ButtonNavigate.tsx
+++ src/components/ButtonNavigate.tsx
@@ -28,19 +28,19 @@ export function ButtonNavigate({ icon, name, url, handleChange }: Props) {
         >
           {name}
         </div>
-        <m.button className="w-full h-full flex items-center justify-center cursor-default">
+        <m.span className="w-full h-full flex items-center justify-center cursor-default">
           <img
             src={`/icons/${icon}.svg`}
             alt="close naviate"
             className={"text-white w-4 group-hover:w-10 transition-all"}
           />
-        </m.button>
+        </m.span>
       </m.a>
     );
   }
 
   return (
-    <m.div
+    <m.button
       onClick={handleChange}
       initial={{ width: "40px", height: "40px" }}
       whileHover={{ width: "65px", height: "65px" }}
@@ -50,20 +50,20 @@ export function ButtonNavigate({ icon, name, url, handleChange }: Props) {
       }}
       className="group w-10 h-10 bg-style-g-2 box-border rounded-full shadow_button_navigate cursor-default"
     >
-      <button
+      <span
         className="absolute -top-9 group-hover:-top-12 -ml-2 transform-50 w-[80px] opacity-0 transition-all \
        group-hover:opacity-100 flex justify-center text-sm border border-style-g-0 \
         rounded-md duration-300 px-1 py-[1px] text-[#7d7d7d] cursor-default"
       >
         {name}
-      </button>
-      <m.button className="w-full h-full flex items-center justify-center cursor-default">
+      </span>
+      <m.span className="w-full h-full flex items-center justify-center cursor-default">
         <img
           src={`/icons/${icon}.svg`}
           alt="close naviate"
           className={"text-white w-4 group-hover:w-10 transition-all"}
         />
-      </m.button>
-    </m.div>
+      </m.span>
+    </m.button>
   );
 }
