cfa122f1c4e98c84835eb449701c70d99894b5b5
higoraln <Higor Allan>
2022-11-06 22:10:05 -0300

    adding theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,8 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
+
+          <meta name="theme-color" content="#161616" />
         </Head>
         <body>
           <Main />
